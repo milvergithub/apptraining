e542eea5a07ddfecc6589d7d743d66d297270eb9
mflores <mflores.transoft@gmail.com>
2020-01-24 11:51:51 -0400

    About tab

--- app/src/main/java/com/transoft/apptraining/ui/main/AboutFragment.java
+++ app/src/main/java/com/transoft/apptraining/ui/main/AboutFragment.java
@@ -4,19 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 import com.transoft.apptraining.R;
 
 public class AboutFragment extends Fragment {
 
   private static final String TAG = "About";
-
   private PageViewModel pageViewModel;
+  private CarouselView carouselView;
+  private int[] images = {R.drawable.autobus, R.drawable.bus_nuevo, R.drawable.bus1, R.drawable.bus2, R.drawable.bus3};
 
   public AboutFragment() {
   }
@@ -36,6 +40,16 @@ public class AboutFragment extends Fragment {
   @Override
   public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View root = inflater.inflate(R.layout.fragment_about, container, false);
+    carouselView = root.findViewById(R.id.carouselView);
+    carouselView.setPageCount(images.length);
+    carouselView.setImageListener(imageListener);
     return root;
   }
+
+  ImageListener imageListener = new ImageListener() {
+    @Override
+    public void setImageForPosition(int position, ImageView imageView) {
+      imageView.setImageResource(images[position]);
+    }
+  };
 }
