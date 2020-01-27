56493453a78f40b28080127942d8e24224e6a2be
mflores <mflores.transoft@gmail.com>
2020-01-27 15:28:29 -0400

    zebra printer integration

---
+++ app/src/main/java/com/transoft/apptraining/ui/main/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.transoft.apptraining.ui.main;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.transoft.apptraining.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_home);
+  }
+}
--- app/src/main/java/com/transoft/apptraining/ui/main/LoginFragment.java
+++ app/src/main/java/com/transoft/apptraining/ui/main/LoginFragment.java
@@ -1,16 +1,17 @@
 package com.transoft.apptraining.ui.main;
 
+import android.content.Intent;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.transoft.apptraining.R;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +20,7 @@ public class LoginFragment extends Fragment {
 
   private static final String TAG = "Login";
   private PageViewModel pageViewModel;
+  private View root;
 
   public LoginFragment() {
     // Required empty public constructor
@@ -39,8 +41,14 @@ public class LoginFragment extends Fragment {
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
-    View root = inflater.inflate(R.layout.fragment_login, container, false);
+    root = inflater.inflate(R.layout.fragment_login, container, false);
+    ButterKnife.bind(this, root);
     return root;
+  }
+
+  @OnClick(R.id.btn_login)
+  public void onLogin() {
+    startActivity(new Intent(root.getContext(), HomeActivity.class));
   }
 
 }
